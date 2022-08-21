affdd4f91cdb0adb88dae1a8290520faec2dbc10
caksodik <caksodik93@gmail.com>
2022-08-21 14:28:34 +0700

    workshop day 1

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const port = 10005
+const prefix = "/"
+const bodyParser = require("body-parser")
+
+const mysql = require("mysql2");
+const pool = mysql.createPool({
+    user: "root",
+    password: "",
+    host: "localhost",
+    port: 3306,
+    database: "unpkediri"
+});
+const mysqlpool = pool.promise();
+app.use(bodyParser.json());
+
+app.get("/mhs", async (req, res) => {
+    let { name } = req.query
+    let [row] = await mysqlpool.query("select * from mahasiswa");
+    console.log(row);
+    res.json(row);
+})
+
+app.post("/mhs", async (req, res) => {
+    try {
+        let name = req.body.name;
+        let npm = req.body.npm;
+        if (!name || !npm) throw new Error("item required");
+        let data = {name, npm}
+        let [row] = await mysqlpool.query("insert into mahasiswa set ? ", data);
+        res.send("ok")
+    } catch (error) {
+        res.status(400).json({
+            message : error.message
+        })
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Halaman Utama")
+})
+
+app.use(prefix, require("./routes/route")());
+
+app.listen(port, () => {
+    console.log("listenig to port " + port)
+})
